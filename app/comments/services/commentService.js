9be9a31e3a9ef1d363e77d6b784b71d21f2f7939
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-18 02:50:52 +0100

    + Make the service a dependency of the comments module instead of the main app.

--- app/comments/services/commentService.js
+++ app/comments/services/commentService.js
@@ -1,7 +1,7 @@
 /**
  * Created by ammyreal on 3/13/15.
  */
-angular.module('CommentsApp')
+angular.module('CommentsModule')
 .factory('CommentService', [function(){
         var data={};
         data.allComment = [];
