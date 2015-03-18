79fdd140cfe1013c0672cc8e9da11bfee02794cb
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-18 01:53:00 +0100

    + Define routes for the application.

--- app/app.js
+++ app/app.js
@@ -1,17 +1,41 @@
 (function () {
   'use strict';
 
-  var app = angular.module('CommentsApp', [
-    //'event',
+  var app = angular.module('app', [
+    'ngRoute',
     'ngMaterial',
+    'calendar',
     'CommentsModule',
     'CommentForm',
     'PostBody'
   ])
-  .config(function ($mdThemingProvider) {
+  .config(function ($routeProvider, $mdThemingProvider) {
+
+    // Define routes for the application
+    $routeProvider
+      .when('/calendar', {
+        templateUrl: './app/calendar/partials/calendar.partial.html',
+        controller: 'MonthController',
+        controllerAs: 'register'
+      })
+      .when('/calendar/:day_id', {
+        templateUrl: './app/events/partials/events.partial.html',
+        controller: 'EventController',
+        controllerAs: 'eventList'
+      })
+      .when('/calendar/:day_id/events/:event_id', {
+        templateUrl: './app/comments/partials/comments.partial.html',
+        controller: 'CommentsController',
+        controllerAs: 'commentCtrl'
+      })
+      .otherwise({
+        redirectTo: '/calendar'
+      });
+
+    // Create a theme for the application
     $mdThemingProvider.theme('default')
-    .primaryPalette('blue-grey')
-    .accentPalette('light-blue');
-  });
+      .primaryPalette('blue-grey')
+      .accentPalette('light-blue');
+    });
 
 }());
