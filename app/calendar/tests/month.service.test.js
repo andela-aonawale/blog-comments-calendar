6cd8aee08657727db095d438870899a40d4fd28b
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-18 13:01:39 +0100

    + Add test specs to cover the 'init' method of the month service.

--- app/calendar/tests/month.service.test.js
+++ app/calendar/tests/month.service.test.js
@@ -5,21 +5,42 @@ describe('MonthService', function () {
 
   // Declare global variables
   var MonthService;
+  var $localStorage;
 
   // Initialize the module
-  beforeEach(module('event'));
+  beforeEach(module('calendar'));
 
   // Initialize the service
   beforeEach(inject(function (_MonthService_) {
     MonthService = _MonthService_;
+    spyOn(MonthService, 'setDays');
   }));
 
 
   // Test suite for populating the service with initial data
   describe('Populate service with months', function () {
 
-    it('it has a null value set to today', function () {
+    it('should contain a $localStorage service', inject(function ($localStorage) {
+      expect($localStorage).not.toBe(null);
+    }));
+
+    it('should have an initial value of null for today', function () {
       expect(MonthService.today).toBe(null);
+    });
+
+    it('should set the value of today to the current day', function () {
+      MonthService.init();
+      expect(MonthService.today).not.toBe(null);
+    });
+
+    it('should create a localStorage item upon initialization', inject(function ($localStorage) {
+      MonthService.init();
+      expect($localStorage.calendar).not.toBe(null);
+    }));
+
+    it('should call the setDays method upon initialization', function () {
+      MonthService.init();
+      expect(MonthService.setDays).toHaveBeenCalled();
     });
   });
 
