0d867e3594f92bcd37db499bc5e02d19a969bf74
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-19 17:22:19 +0100

    + Add test spec for month service.

--- app/calendar/tests/month.service.test.js
+++ app/calendar/tests/month.service.test.js
@@ -28,9 +28,15 @@ describe('MonthService', function () {
       expect(MonthService.today).toBe(null);
     });
 
-    it('should set the value of today to the current day', function () {
+    it('should set a value to today when initialized', function () {
       MonthService.init();
       expect(MonthService.today).not.toBe(null);
+    });
+
+    it('should set the value of today to the current day', function () {
+      var today = moment();
+      MonthService.init();
+      expect(MonthService.today.date()).toEqual(today.date());
     });
 
     it('should create a localStorage item upon initialization', inject(function ($localStorage) {
@@ -42,6 +48,46 @@ describe('MonthService', function () {
       MonthService.init();
       expect(MonthService.setDays).toHaveBeenCalled();
     });
+  });
+
+  // Test suite to check the month names
+  describe('Get month names', function () {
+
+    it('should return "January" from an array of month names', function () {
+      expect(MonthService.getMonths()[0]).toEqual('January');
+    });
+
+    it('should return "August" from an array of month names', function () {
+      expect(MonthService.getMonths()[7]).toEqual('August');
+    });
+  });
+
+  // Test suite to check that the current month is set and is accurate
+  describe('Check the current month', function () {
+
+    it('should ')
+  });
+
+  // Test suite to check that the current day is set and is accurate
+  describe('Check the current day', function () {
+
+    it('should have the current day set to today upon initialization', function () {
+      var today = moment();
+      MonthService.init();
+      expect(MonthService.getCurrentDay()).toEqual(today.date());
+    });
+
+    it('should be able to set the current date to a specified value', function () {
+      MonthService.init();
+      MonthService.setCurrentDay(11);
+      expect(MonthService.getCurrentDay()).toEqual(11);
+    });
+
+    it('should return the day of the week for the first day of the current month', function () {
+      MonthService.init();
+      MonthService.setCurrentMonth();
+    });
+
   });
 
 });
