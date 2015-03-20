ffa1548aa9c133ef1218e5a08ac1121865d173a8
andela-ioraelosi <ifeanyi.oraelosi@andela.co>
2015-03-20 02:56:35 +0100

    + Add method to navigate to the event page.

--- app/events-list/controllers/event.list.controller.js
+++ app/events-list/controllers/event.list.controller.js
@@ -3,22 +3,32 @@
  */
 angular.module('eventlist')
     .controller('EventListController', ['$mdDialog', 'MonthService', function($mdDialog, MonthService){
-    this.date = new Date();
-    function Makedialog(){
-        return function(ev){
-            $mdDialog.show({
-                controller: 'NewEventController as newEventCtrl',
-                templateUrl: './app/events-list/partials/add-event.html',
-                targetEvent: ev
+        var eventList = this;
 
-            });
+        eventList.currentMonth = MonthService.getCurrentMonth();
+        eventList.currentDay = MonthService.getCurrentDay();
+
+        function Makedialog(){
+            return function(ev){
+                $mdDialog.show({
+                    controller: 'NewEventController as newEventCtrl',
+                    templateUrl: './app/events-list/partials/add-event.html',
+                    targetEvent: ev
+
+                });
+            };
+        }
+
+        eventList.events = MonthService.getEvents();
+        eventList.currentDay = MonthService.getCurrentDay();
+        eventList.Months = MonthService.getMonths();
+        eventList.currentMonth = this.Months[MonthService.getCurrentMonth()];
+
+
+        eventList.showEventDialog = Makedialog();
+
+        eventList.goToEvent = function (eventID) {
+            MonthService.goToEvent(eventList.currentMonth, eventList.currentDay, eventID);
         };
-    }
-        this.events = MonthService.getEvents();
-        this.currentDay = MonthService.getCurrentDay();
-        this.Months = MonthService.getMonths();
-        this.currentMonth = this.Months[MonthService.getCurrentMonth()];
 
-
-    this.showEventDialog = Makedialog();
 }]);
